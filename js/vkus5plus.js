0f087db86c55d6c466a1fd30b21677c0cf669dc3
Mikhail Gerasimov <mgerasim@inbox.ru>
2014-08-29 17:06:25 +1100

    itemsub_selected

--- js/vkus5plus.js
+++ js/vkus5plus.js
@@ -1,8 +1,22 @@
 $(function(){
-   $('a[href^="#"]').click(function(){
-	   
+   $('a[href^="#"]').click(function(){	   
         var target = $(this).attr('href');
         $('html, body').animate({scrollTop: $(target).offset().top}, 300);
         return false; 
    }); 
-});
+   
+  
+});	
+
+$(document).ready(function () {
+     $('.itemsub').click(function(){
+	   
+/*	   alert( $('#itemsub_grechka').children('.itemsub_selected').css('display') );*/
+	   if ($(this).children('.itemsub_selected').first().css('display') == 'none')
+	   {
+			$(this).children('.itemsub_selected').show();
+	   } else {
+		   $(this).children('.itemsub_selected').hide();
+	   }
+   });
+}); 
