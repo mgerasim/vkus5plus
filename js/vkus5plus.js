5fe9b4e8c6b2b927247924a081e979f6d5cbaaed
Mikhail Gerasimov <mgerasim@inbox.ru>
2014-09-02 14:05:36 +1100

    VP0002

--- js/vkus5plus.js
+++ js/vkus5plus.js
@@ -10,13 +10,13 @@ $(function(){
 
 $(document).ready(function () {
      $('.itemsub').click(function(){
-	   
-/*	   alert( $('#itemsub_grechka').children('.itemsub_selected').css('display') );*/
 	   if ($(this).children('.itemsub_selected').first().css('display') == 'none')
 	   {
 			$(this).children('.itemsub_selected').show();
+//			$(this).css("border","#c4c4c4 solid 2px");
 	   } else {
 		   $(this).children('.itemsub_selected').hide();
+//	   		$(this).css("border","#ffffff solid 2px");
 	   }
    });
 }); 
