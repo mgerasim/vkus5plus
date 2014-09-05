f7cf34f635708813e4df62028c20d559d7785147
Mikhail Gerasimov <mgerasim@inbox.ru>
2014-09-05 19:31:55 +1100

    selected

--- js/vkus5plus.js
+++ js/vkus5plus.js
@@ -8,15 +8,27 @@ $(function(){
   
 });	
 
+function addOrderPosition(divItemsubSelected) {
+
+	alert( $('.itemsub_selected').length );
+}
+
+function removeOrderPosition(divItemsubSelected) {
+}
+
 $(document).ready(function () {
      $('.itemsub').click(function(){
-	   if ($(this).children('.itemsub_selected').first().css('display') == 'none')
+	   if ($(this).children('.itemsub_mark').first().css('display') == 'none')
 	   {
-			$(this).children('.itemsub_selected').show();
+			$(this).children('.itemsub_mark').show();
 //			$(this).css("border","#c4c4c4 solid 2px");
+			$(this).addClass("itemsub_selected");
+
+			addOrderPosition( $(this) );
 	   } else {
-		   $(this).children('.itemsub_selected').hide();
-//	   		$(this).css("border","#ffffff solid 2px");
+		   $(this).children('.itemsub_mark').hide();
+	   		$(this).css("border","#ffffff solid 2px");
+			$(this).removeClass("itemsub_selected");
 	   }
    });
 }); 
