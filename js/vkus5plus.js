ccbe75f24de5519499405f4bcbed415b906a9ca1
Mikhail Gerasimov <mgerasim@inbox.ru>
2014-09-11 13:09:32 +1100

    order

--- js/vkus5plus.js
+++ js/vkus5plus.js
@@ -10,10 +10,95 @@ $(function(){
 
 function addOrderPosition(divItemsubSelected) {
 
-	alert( $('.itemsub_selected').length );
+	if ( $('.itemsub_selected').length == 1 ) {
+		var itemOrder = jQuery('<div/>', {
+			id: 'item_orders',
+			class: 'item'
+		});
+		$('#content').append(itemOrder);
+		
+		var itemHeader = jQuery('<div/>', {
+			class: 'itemheader',
+			text: 'Ваш заказ'
+		});		
+		itemOrder.append(itemHeader);
+		
+		var itemsubOrder = jQuery('<div/>', {
+			id: 'itemsub_orders',
+			class: 'itemsub',
+			text: 'Ваш заказ'
+		});		
+		itemOrder.append(itemsubOrder);
+		
+		var clearfix = jQuery('<div/>', {
+			class: 'clearfix'
+		});		
+		itemOrder.append(clearfix);
+		
+		
+		var tableOrder = jQuery('<table/>', {
+		});
+		itemsubOrder.append(tableOrder);
+				
+		var trOrder = jQuery('<tr/>', {
+		});
+		tableOrder.append(trOrder);
+		
+		var thOrder = jQuery('<th/>', {
+			text: '№'
+		});
+		trOrder.append(thOrder);
+				
+		thOrder = jQuery('<th/>', {
+			text: 'Наименование'
+		});
+		trOrder.append(thOrder);
+		
+		thOrder = jQuery('<th/>', {
+			text: 'Цена'
+		});
+		trOrder.append(thOrder);
+	}
+					
+	var trOrder1 = jQuery('<tr/>', {
+		id: divItemsubSelected.attr('id')+'order'
+	});
+	var tableOrder1 = $('#itemsub_orders table');
+	tableOrder1.append(trOrder1);
+	
+	var tdOrder1 = jQuery('<td/>', {
+		text: $('.itemsub_selected').length
+	});
+	trOrder1.append(tdOrder1);
+						
+	tdOrder1 = jQuery('<td/>', {
+		text: divItemsubSelected.find('.itemsub_caption').text()
+	});
+	trOrder1.append(tdOrder1);
+	
+	tdOrder1 = jQuery('<td/>', {
+		text: divItemsubSelected.find('.itemsub_price').text()
+	});
+	trOrder1.append(tdOrder1);
+	
+	$('#itemsub_orders table').append(trOrder1);
+	
 }
 
 function removeOrderPosition(divItemsubSelected) {
+	
+	if ( $('.itemsub_selected').length == 0 ) {
+		$('#item_orders').remove();
+	}
+	
+	$('#'+divItemsubSelected.attr('id')+'order').remove();
+	
+
+	$('#itemsub_orders table tr').each(function(index){
+		if (index != 0) {
+			$(this).children().first().text(index);
+		}		
+	});
 }
 
 $(document).ready(function () {
@@ -27,8 +112,9 @@ $(document).ready(function () {
 			addOrderPosition( $(this) );
 	   } else {
 		   $(this).children('.itemsub_mark').hide();
-	   		$(this).css("border","#ffffff solid 2px");
+//	   		$(this).css("border","#ffffff solid 2px");
 			$(this).removeClass("itemsub_selected");
+			removeOrderPosition( $(this) );
 	   }
    });
 }); 
